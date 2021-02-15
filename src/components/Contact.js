a8b42595283fbe18aef942fd929bedcae72aad81
markusmatilainen <markus.matilainen@edu.savonia.fi>
2021-02-15 16:16:47 +0200

    Changed styling and content

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,37 +1,38 @@
 import React from "react";
 import '../App.css';
+import { FaPhone } from 'react-icons/fa';
+import { FaStreetView } from 'react-icons/fa'
+import { FaCity } from 'react-icons/fa'
 
 function Contact() {
   return (
-    <div>
+    <div className="contactareas">
       <div className="contactarea1" id="contact">
         <h2>Contact Us</h2>
-        <div className="contactinfo">
-          <p>Phone: 123456789</p>
-          <p>95 Viale Eritrea</p>
-          <p>Rome, Lazio</p>
-        </div>
+        <p><FaPhone className="contact-icon" /> 123456789</p>
+        <p><FaStreetView className="contact-icon" /> 95 Viale Eritrea</p>
+        <p><FaCity className="contact-icon" /> Rome, Lazio</p>
       </div>
-      <div className="contactarea2" id="form">
+      <div className="contactarea2">
         <h2>Contact Form</h2>
-        <form className="contactform">
+        <form className="contactform" id="form">
           <div className="formcontainer">
-            <label for="fname">First name:</label>
+            <label for="fname">First name</label>
             <input type="text" id="fname" name="fname" />
           </div>
           <div className="formcontainer">
-            <label for="lname">Last name:</label>
+            <label for="lname">Last name</label>
             <input type="text" id="lname" name="lname" />
           </div>
           <div className="formcontainer">
-            <label for="email">Email Address:</label>
+            <label for="email">Email</label>
             <input type="email" id="email" name="email" />
           </div>
           <textarea id="message" name="message" placeholder="Write your message here..." />
           <input type="submit" value="Submit" />
-          <input type="reset" />
+          <input type="reset" value="Reset" />
         </form>
-      </div>
+      </div>   
     </div>
   );
 }
